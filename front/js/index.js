9c0dc59370afffe86336a4b3a0daba6b9cfb66f6
KotaroFurusawa <kotaro.furusawa@clipline.jp>
2022-08-10 17:06:50 +0900

    ユーザ登録機能の追加

--- front/js/index.js
+++ front/js/index.js
@@ -1,10 +1,22 @@
+const env = {
+    "HOST": "",
+    "USER_DATA": "udata"
+}
+
 const App = new Vue({
     el: '#app',
     data() {
         return {
             flg: false,
             num: "",
-            nums: []
+            user_data: {},
+            nums: [],
+            create_user_flg: false,//本当はfalse
+            conf_user: false,//本当はfalse
+            discord_id: "",
+            create_user_msg: { "color": "", "text": "" },
+            explain_flg: false,
+            user_name: ""
         }
     },
     mounted() {
@@ -22,20 +34,19 @@ const App = new Vue({
             }
             return false;
         },
-        input_num: function (key) {
+        input_num: async function (key) {
             if (this.num.length < 7) {//入力が7文字未満なら
                 this.num = this.num + String(key);//1文字追加
             } else if (this.num.length === 7) {//入力が七文字なら
-                let time = new Date().toLocaleString({ timeZone: 'Asia/Tokyo' });
-                this.num = this.num + String(key);//ラスト一文字追加
+                let time = await new Date().toLocaleString({ timeZone: 'Asia/Tokyo' });
+                this.num = await this.num + String(key);//ラスト一文字追加
+                //this.user_data = await this.getUserData(this.num);//ユーザ問い合わせ．後々、エラー取得処理も加える
+                //ここでifで登録処理を行う
+                if (this.user_data === "user_not_found") {
+                    this.create_user_flg = true;
+                }
+                //ここで通知処理を行う
                 this.nums.push({ time: time, num: this.num });
-                /*
-                
-                let text_color = "#00ff00";
-                let status = "INFO";
-                let log = `<p class="mx-3" style="color:${text_color}">${new Date().toLocaleString({ timeZone: 'Asia/Tokyo' })} [${status}]    :${num} さん入室</p>`;
-                document.getElementById('console').insertAdjacentHTML('beforeend', log);//ログ表示
-                */
                 this.flg = false;//読み取りを終了する
                 this.num = "";//numをリセットする
             }
@@ -45,6 +56,64 @@ const App = new Vue({
             if (key === '%') {
                 this.flg = true;
             }
+        },
+
+        getUserData: function (id) {
+            //api作成後修正
+            let params = { params: { id: id } };
+            let url = env.HOST + env.USER_DATA;
+            axios
+                .get(url, params)
+                .then(res => {
+                    return res.data.code == 200 ? res.data.data : res.data.message;
+                });
+            //.catch()で通信エラー時の処理、.finally()で成功・失敗時共通の最終処理を書く
+            //ユーザがいないときは"user_not_found"を返す
+        },
+
+        searchUser: async function () {
+            if (await this.inputCheck()) {//入力フォームチェック
+                //this.user_name = await this.getDiscordName(this.discord_id);//ユーザネーム取得
+                this.createUserClose();//ユーザ登録画面を閉じる
+                this.conf_user = true;//ユーザ確認画面を表示
+            }
+        },
+        inputCheck: function () {
+            //入力フォームの確認
+            if (this.discord_id == "") {
+                this.create_user_msg = { "color": "text-danger", "text": "＊idを入力してください。" };
+                return false;
+            } else if (!this.discord_id.match("#[0-9]{4}$")) {
+                this.create_user_msg = { "color": "text-danger", "text": "＊idは数字4桁で入力してください。" };
+                return false;
+            } else {
+                this.create_user_msg = { "color": "", "text": "ユーザ検索中..." };
+                return true;
+            }
+        },
+
+        getDiscordName: function (discord_id) {
+            let params = { params: { id: id } };
+            let url = env.HOST + env.DISCORD_NAME;
+            axios
+                .get(url, params)
+                .then(res => {
+                    return res.data.code == 200 ? res.data.data : res.data.message;
+                });
+        },
+        createUser: function () {
+            //apiを用いてユーザ登録
+
+            this.conf_user = false;//ユーザ確認画面を閉じる
+
+            //通知処理を行い、ログを表示
+        },
+
+        createUserClose: function () {
+            //ユーザ登録画面を閉じて初期化する。
+            this.create_user_flg = false;//ユーザ登録画面を閉じる
+            this.create_user_msg = { "color": "", "text": "" };//ユーザ登録画面の初期化
+            this.explain_flg = false;//ユーザ登録画面の初期化
         }
     },
 
